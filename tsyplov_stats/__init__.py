b0645c92b2194a5d81a5b23a0251ec4a13bec57c
Алексей <43031739+tsyploff@users.noreply.github.com>
2020-04-28 16:24:44 +0300

    Add invboxcoc, cross_validation_split

--- tsyplov_stats/__init__.py
+++ tsyplov_stats/__init__.py
@@ -1,2 +1,17 @@
 
-__all__ = ('arma_model', 'autoregression_model', 'fejer_sums', 'linearregression_model', 'wolfram_functions')
+__all__ = ['arma_model', 'autoregression_model', 'fejer_sums', 'linearregression_model', 'polynomialregression_model', 'wolfram_functions']
+
+from tsyplov_stats.wolfram_functions import *
+
+def invboxcox(ts, lmbda):
+    if lmbda != 0:
+        return np.exp(np.log(lmbda*ts + 1) / lmbda)
+    else:
+        return np.exp(ts)
+
+def cross_validation_split(ts, n):
+	'''Gives the list of tuples (train, test), where len(test) is always equal n'''
+	result = []
+	for i in range(len(ts), len(ts) - (n - 1)*(len(ts)//n), -n):
+		result.append(take_drop(ts[:i], n))
+	return result[::-1]
